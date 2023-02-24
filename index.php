64cb5064eb298f486b3348cf5782a04132e532a5
mariolip <117418756+mariolip@users.noreply.github.com>
2023-02-23 19:09:37 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
+<?php
+$servername = "localhost";
+$username = "mariolip";
+$password = "Pneuma423";
+$dbname = "pokemondatabase";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully. </br>";
+
+$sql = "SELECT * FROM pokedex";
+$result = $conn->query($sql);
+?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -62,6 +80,40 @@ img.imgSize {
   height: 100%;
   border-radius: 50%;
 }
+* {
+    margin:0;
+    padding:0;
+}
+.dexButton button {
+  background-color: #ad8351;
+  color: #222224;
+  border-color: #6e5334;
+  border-width: 4px;
+  cursor:grab;
+  width: 44px;
+  height: 180px;
+  display: inline;
+  border-radius: 10%;
+  border-top: none;
+  border-bottom: none;
+  padding-top: 15px;
+  padding-right: 2px;
+}
+.dexButton p{
+  font-family: "Asap Condensed", Trebuchet MS, sans-serif!important;
+	font-weight: 500;
+  font-size: 14pt;
+  text-align: left;
+  text-indent: -55px;
+  transform:rotate(-90deg);
+    }
+.dexButton button:hover {
+  background-color: #af6f48;
+  border-color: #473622;
+  cursor: grab;
+}
+.dexButton button:active{
+}
 form {text-align: center;}
 
 </style>
@@ -80,25 +132,29 @@ form {text-align: center;}
     </div>
   </head>
   <body>
-    <br></br>
-    <form action="UserTest.php" method="post">
-        <div class="interfaceButton">
-            <label for="uname"><b>Username: </b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required>
-            <br></br>
-            <label for="psw"><b>Password: </b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required>   
-            <br></br>
-            <label>
-                <input type="checkbox" checked="checked" name="remember"> Remember me
-            </label>
-            <br></br>
-            <button type="submit" class="inferfaceButtonContent">Sign In</button>
-        </div>
-        <br></br>
-        <div class="interfaceButton">
-            <button type="button" class="inferfaceButtonContent">Forgot Password?</button>
-        </div>
-    </form>
-  </body>
+  <div class="dexButton">
+<?php
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "<br>";
+    echo "Dex Number: " . $row["DexNum"]."<br>";
+    echo "Name: " . $row["PokemonName"]."<br>";
+    echo "Primary Type: " . $row["TypeOne"]."<br>";
+    echo "Secondary Type: " . $row["TypeTwo"]."<br>";
+    echo "HP: " . $row["HP"]."<br>";
+    echo "Attack: " . $row["Attack"]."<br>";
+    echo "Defense: " . $row["Defense"]."<br>";
+    echo "Special Attack: " . $row["Sp. Attack"]."<br>";
+    echo "Special Defense: " . $row["Sp. Def"]."<br>";
+    echo "Speed: " . $row["Speed"]."<br>";
+    echo "<button type='button' onclick=\"window.location.href = 'https://pokemondb.net/pokedex/" . $row["refName"] . "';\"><p>" . $row["DexNum"] . ": " . $row["PokemonName"] . "</p><p style='text-align:left;'><img src='https://img.pokemondb.net/sprites/scarlet-violet/normal/" . $row["refName"] . ".png' style='transform:rotate(90deg);padding:10px;padding-right:5px;width:36px;height:36px;'></p></button></br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+</div>
+</body>
 </html>
